2370aa01e426127f3d961c207796629fba7e7b96
wilson <wilsonxavier_29@hotmail.com>
2020-10-14 23:21:58 -0500

    mi primer commit

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.views.generic.base import TemplateView
+
+
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title': 'CHAT UNIVERSITARIO UTM'})
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
